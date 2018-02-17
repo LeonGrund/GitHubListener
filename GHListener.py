de50d25b6021656c91ee994696d2e8d5cd8b0bcb
Leon <LeonCapGrund@gmail.com>
2018-02-17 14:39:21 -0600

    Update GHListener.py
    space fix

--- GHListener.py
+++ GHListener.py
@@ -34,10 +34,9 @@ if __name__ == "__main__":
 	print('registering signal')
 	signal.signal(signal.SIGINT, signal_handler)
 	print('registering signal -- done')
-
-        # create an INET, STREAMing socket
+	# create an INET, STREAMing socket
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # bind the socket to a public host, and a well−known port
+	# bind the socket to a public host, and a well−known port
 	serversocket.bind((hostname, port))
 
 	serversocket.listen(5)
